a1e5e2f922120a21ca33180b4f9e3d50b2a34058
Nolan Kohler <kohlern@wlha.wlwv.k12.or.us>
2019-10-03 09:23:30 -0700

    First commit.

---
+++ legalDrink.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class legalDrink{
+  public static void main(String[] args){
+    int age;
+    Scanner scan = new Scanner(System.in);
+    System.out.println
+  }
+}
